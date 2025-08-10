ec2a1b17067e0e3da63d4a77c04af70230e494df
ItsmeLee <arsmagica90@gmail.com>
2025-08-10 15:12:47 +0900

    헤더 href 수정

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,10 +6,10 @@ export default function Header() {
         <header className="border-b">
           <div className="container mx-auto flex justify-between h-14 px-20 max-md:flex-col max-md:h-20 max-md:items-center">
             <div className="flex items-center gap-4">
-            <Link href="/" className="font-semibold">My MDX Blog</Link>
+            <Link href="/blog" className="font-semibold">My MDX Blog</Link>
             </div>
             <nav className="flex gap-4 text-sm items-center">
-              <Link href="/blog" className="hover:underline">Blog</Link>
+              <Link href="/About" className="hover:underline">About</Link>
               {/* github icon으로 대체할 예정 */}
               <a href="https://github.com" className="hover:underline" target="_blank" rel="noreferrer">GitHub</a>
               <ModeToggle />

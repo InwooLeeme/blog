207d3b8418a4d94f75bc2694e36a92e7ca4ecaf2
ItsmeLee <arsmagica90@gmail.com>
2025-08-11 18:55:16 +0900

    ToC 컴포넌트 준비

---
+++ src/app/components/mdx/PostTOC.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import * as React from "react"
+
+type TocItem = { id: string; text: string; level: number }
+
+export default function PostTOC() {
+    return (
+        <div></div>
+    )
+}

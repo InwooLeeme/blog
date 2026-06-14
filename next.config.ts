a5e033fea676d2187668d387eabc2774477e6912
ItsmeLee <arsmagica90@gmail.com>
2026-06-14 16:07:44 +0900

    perf: simple-icons import 최적화로 사용 아이콘만 번들

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // simple-icons 사용한 아이콘만 번들되도록 import 최적화
+    optimizePackageImports: ["simple-icons"],
+  },
 };
 
 export default nextConfig;
